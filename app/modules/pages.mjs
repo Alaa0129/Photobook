5553c0544e52f4121a20fb4f308887a9d2a49656
mfoman <frederikblunck@gmail.com>
2020-05-15 11:26:48 +0200

    fix broken js

--- app/modules/pages.mjs
+++ app/modules/pages.mjs
@@ -1,84 +1,8 @@
-/**
- * Storage
- * Author: Frederik Søgren Blunck Raisa
- */
-export default {
-  storage: window.localStorage,
-
-  addPage,
-  setPage,
-  getPage,
-  getPages,
-
-  setState,
-  getState
+export default function init({storage}) {
+  storage.setState(defaultState)
 }
 
-
-/**
- * API
- */
-function addPage(page) {
-  let state = getState()
-
-  state.pages.push(page)
-
-  setState(state)
-}
-
-function setPage(pageNumber, pageContent) {
-  let state = getState()
-
-  state.pages[pageNumber] = pageContent
-
-  setState(state)
-}
-
-function delPage(pageNumber = null) {
-  let state = getState()
-
-  if (pageNumber) {
-    state.pages.splice(pageNumber)
-  } else {
-    state.pages.pop()
-  }
-
-  setState(state)
-}
-
-function getPage() {
-  throw Error('Placeholder')
-}
-
-function getPages(pageNumber = null) {
-  let state = getState()
-
-  if (pageNumber) {
-    return state.pages[pageNumber]
-  }
-  return state.pages
-}
-
-/**
- * Helper functions
- */
-function setState(newState) {
-  if (typeof newState === 'function') {
-    const oldState = getState()
-    appstate = newState(oldState)
-  } else {
-    appstate = newState
-  }
-}
-
-function getState() {
-  return appstate
-}
-
-/**
- * Storage keeping
- */
-let appstate = {
+let defaultState = {
   title: "Min fotobog",
   pages: [
     {
